3f3af79c56973cd5d00a891d09c263f8c3356c30
JPJDoran <44862368+JPJDoran@users.noreply.github.com>
2020-03-02 08:19:52 +0000

    Removed comments

--- application/config/email.php
+++ application/config/email.php
@@ -1,18 +1,11 @@
 <?php
 
-// $config['protocol'] = 'smtp';
-// $config['smtp_host'] = 'smtp.sendgrid.net';
-// $config['smtp_user'] = 'apikey';
-// $config['smtp_pass'] = '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';
-// $config['smtp_port'] = 25;
 $config['crlf'] = "\r\n";
 $config['newline'] = "\r\n";
 
 $config['mailpath'] = '/usr/sbin/sendmail';
 $config['protocol'] = 'sendmail';
-// $config[‘smtp_host’] = “relay-hosting.secureserver.net”;
-// $config[‘smtp_user’] = “youremailuser@godaddydomain.com”;
-// $config[‘smtp_pass’] = “yourpassword”;
+
 $config['smtp_port'] = '25';
 $config['mailtype'] = 'html';
 
